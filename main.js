e43fbd831c91f2653c5d925a482fc1301b5278f1
clarababette <clara_coetzee@zoho.com>
2022-03-23 15:43:26 +0200

    basic styling added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css';
+import './style.scss';
 import { Route } from './taxi-rank';
 
 const taxiRankRoutes = [
@@ -26,33 +26,38 @@ const taxiRankRoutes = [
 const taxiRank = new Route(taxiRankRoutes);
 
 const routeElems = taxiRank.routes.map((route) => {
- return `<tr id="${route.destination}">
-    <td class="destination">${route.destination}</td>
-    <td class="people"><button class="remove_person">-</button><span>${route.people}</span><button class="add_person">+</button></td>
-    <td class="taxis"><span>${route.taxis}</span><button class="add_taxi">+</button></td>
-    <td class="activity">${route.activity}</td>
-  </tr>`
+  const activity = route.activity.map(act => `<div>${act}</div>`).join(' ');
+ return `<div class="route" id="${route.destination}">
+    <div class="destination">${route.destination}</div>
+    <div class="people">
+      <div>Passengers waiting</div>
+      <button class="remove_person material-icons-round">remove_circle</button>
+      <span>${route.people}</span>
+      <button class="add_person material-icons-round">add_circle</button></div>
+    <div class="taxis">
+      <div>Taxis ready to depart</div>
+        <span>${route.taxis}</span>
+        <button class="add_taxi material-icons-round">add_circle</button></div>
+    <div class="activity"><div class="material-icons-round">
+airport_shuttle
+</div><div><span>${activity}</span></div></div>
+  </div>`
 })
 
-const rankElem = `<table class="queues">
-                      <tr>
-                        <th>Destination</th>
-                        <th>Passenger Queue</th>
-                        <th>Taxi Queue</th>
-                        <th>Activity</th>
-                       </tr>
+const rankElem = `<div class="routes">
                        ${routeElems.join(' ')}
-                    </table>`
+                    </div>`
 
 document.querySelector('#app').innerHTML = rankElem;
 
 const updateQueueElem = (queue) => {
+  const activity = queue.activity.map(act => `<div>${act}</div>`).join(' ');
   document.querySelector(`#${queue.destination} .people span`).innerHTML = queue.people;
   document.querySelector(`#${queue.destination} .taxis span`).innerHTML = queue.taxis;
-  document.querySelector(`#${queue.destination} .activity`).innerHTML = queue.activity;
+  document.querySelector(`#${queue.destination} .activity span`).innerHTML = activity;
 }
 
-document.querySelector('.queues').addEventListener('click', (event) => {
+document.querySelector('.routes').addEventListener('click', (event) => {
   const route = event.target.parentNode.parentNode.id;
   const action = event.target.classList[0];
   taxiRank.queue = route;
