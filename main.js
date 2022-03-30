e7cc204e1c161408816c587774b679f55c88c9ab
clarababette <clara_coetzee@zoho.com>
2022-03-30 10:57:10 +0200

    travis set up

--- main.js
+++ main.js
@@ -1,80 +1,150 @@
 import './style.scss';
-import { Route } from './taxi-rank';
+import {Route} from './taxi-rank';
+import Handlebars from 'handlebars';
 
 const taxiRankRoutes = [
   {
-      destination: 'Makhaza',
-      people: 12,
-      taxis: 0,
-      activity: []
+    destination: 'Makhaza',
+    people: [],
+    taxis: [],
+    departed: [],
   },
   {
-      destination: 'Belhar',
-      people: 34,
-      taxis: 0,
-      activity: []
+    destination: 'Belhar',
+    people: [],
+    taxis: [],
+    departed: [],
   },
   {
-      destination: 'Bellville',
-      people: 23,
-      taxis: 0,
-      activity: []
+    destination: 'Bellville',
+    people: [],
+    taxis: [],
+    departed: [],
   },
-  
-]
+];
 
-const taxiRank = new Route(taxiRankRoutes);
+const nextTaxiSource = `<div class="next-to-depart">
+                          {{#if this.nextTaxi}}
+                          <div class="ready"><div>Next taxi to depart</div><div class="taxi-icon material-icons-round">airport_shuttle</div><div>arrived @ {{this.nextTaxi}}</div></div>
+                          {{else}}
+                          <div>There are no available taxis at the moment.</div>
+                          {{/if}}
+                        </div>`;
+const peopleQueueSource = `<div class="people-queue">
+                        <div>Passenger Queue</div>
+                        <button class="remove_person material-icons-round">remove_circle</button>
+                        <span class="num-people">{{this.numPeople}}</span>
+                        <button class="add_person material-icons-round">add_circle</button></div>`;
+const taxiQueueSource = `<div class="taxi-queue">
+                        <div>Taxi Queue</div>
+                        <div class="num-taxis">{{this.numTaxisWaiting}}</div>
+                        <button class="add_taxi">Add taxi to queue</button></div>`;
+const waitTimeSource = `<div class="wait-time">
+                        <div>Average passenger wait time</div>
+                        <div class="time">{{this.avgWaitTime}}</div>
+                        </div>`;
+const departedTaxisSource = `<div class="departed-taxis">
+                        <div>No. of taxis departed in the last hour</div>
+                        <div class="num-departed">{{this.numTaxisDeparted}}</div>
+                        </div>`;
 
-const routeElems = taxiRank.routes.map((route) => {
-  const activity = route.activity.map(act => `<div>${act}</div>`).join(' ');
- return `<div class="route" id="${route.destination}">
-    <div class="destination">${route.destination}</div>
-    <div class="people">
-      <div>Passengers waiting</div>
-      <button class="remove_person material-icons-round">remove_circle</button>
-      <span>${route.people}</span>
-      <button class="add_person material-icons-round">add_circle</button></div>
-    <div class="taxis">
-      <div>Taxis ready to depart</div>
-        <span>${route.taxis}</span>
-        <button class="add_taxi material-icons-round">add_circle</button></div>
-    <div class="activity"><div class="material-icons-round">
-airport_shuttle
-</div><div><span>${activity}</span></div></div>
-  </div>`
-})
+const routeSource = `<div class="destination">{{destination}}</div>
+${nextTaxiSource}
+${peopleQueueSource}
+${taxiQueueSource}
+${waitTimeSource}
+${departedTaxisSource}`;
 
-const rankElem = `<div class="routes">
-                       ${routeElems.join(' ')}
-                    </div>`
+const allRoutesSource = `{{#each routes}}
+<div class="route" id="{{this.destination}}">${routeSource}</div>
+{{/each}}`;
 
-document.querySelector('#app').innerHTML = rankElem;
+const routeTemplate = Handlebars.compile(routeSource);
+const allRoutesTemplate = Handlebars.compile(allRoutesSource);
 
-const updateQueueElem = (queue) => {
-  const activity = queue.activity.map(act => `<div>${act}</div>`).join(' ');
-  document.querySelector(`#${queue.destination} .people span`).innerHTML = queue.people;
-  document.querySelector(`#${queue.destination} .taxis span`).innerHTML = queue.taxis;
-  document.querySelector(`#${queue.destination} .activity span`).innerHTML = activity;
-}
+const taxiRank = localStorage.getItem('routes')
+  ? new Route(JSON.parse(localStorage.getItem('routes')))
+  : new Route(taxiRankRoutes);
+
+document.querySelector('#app').innerHTML = `<div class="routes">
+                       ${allRoutesTemplate({
+                         routes: taxiRank.detailedRoutes,
+                       })} </div>`;
+
+const updateWaitTime = () => {
+  taxiRank.detailedRoutes.forEach((route) => {
+    document.querySelector(`#${route.destination} .time`).innerHTML =
+      route.avgWaitTime;
+  });
+};
+
+setInterval(updateWaitTime, 60000);
+
+const updatePersonQueueElem = (people, destination) => {
+  document.querySelector(`#${destination} .num-people`).innerHTML = people;
+};
+const updateTaxiQueueElem = (taxis, destination, nextTaxi) => {
+  document.querySelector(`#${destination} .num-taxis`).innerHTML = taxis;
+  const notOnly = document
+    .querySelector(`#${destination} .next-to-depart`)
+    .firstElementChild.classList.contains('ready');
+  if (!notOnly) {
+    document.querySelector(
+      `#${destination} .next-to-depart`,
+    ).innerHTML = `<div class="ready"><div>Next taxi to depart</div><div class="taxi-icon material-icons-round">airport_shuttle</div><div>arrived @ ${nextTaxi}</div></div>`;
+  }
+};
+
+const wait = false;
 
 document.querySelector('.routes').addEventListener('click', (event) => {
   const route = event.target.parentNode.parentNode.id;
   const action = event.target.classList[0];
+  const buttons = ['.remove_person', '.add_person', '.add_taxi'];
+  buttons.forEach((button) => {
+    document.querySelector(`#${route} ${button}`).disabled = true;
+  });
   taxiRank.queue = route;
+  const prevDeparted = taxiRank.queue.numTaxisDeparted;
   switch (action) {
     case 'remove_person':
-      taxiRank.removePerson();
+      updatePersonQueueElem(taxiRank.removePerson(), route);
       break;
     case 'add_person':
-      taxiRank.addPerson();
+      updatePersonQueueElem(taxiRank.addPerson(), route);
       break;
     case 'add_taxi':
       taxiRank.addTaxi();
+      updateTaxiQueueElem(
+        taxiRank.queue.numTaxisWaiting,
+        route,
+        taxiRank.queue.nextTaxi,
+      );
       break;
     default:
       break;
   }
-  console.log(taxiRank.queue)
-  updateQueueElem(taxiRank.queue)
-  console.log(taxiRank.routes)
-})
+
+  const departure = new Promise((resolve, reject) => {
+    if (taxiRank.queue.numTaxisDeparted > prevDeparted) {
+      document.querySelector(
+        `#${route} .next-to-depart`,
+      ).innerHTML = `<div class="departing"><div>DEPARTING</div><div class="taxi-icon material-icons-round">airport_shuttle</div><div>NOW</div></div>`;
+      setTimeout(() => {
+        document.querySelector(`#${taxiRank.queue.destination}`).innerHTML =
+          routeTemplate(taxiRank.queue);
+        resolve();
+      }, 5000);
+    } else {
+      resolve();
+    }
+  });
+
+  departure.then(() => {
+    buttons.forEach(
+      (button) =>
+        (document.querySelector(`#${route} ${button}`).disabled = false),
+    );
+  });
+  localStorage.setItem('routes', JSON.stringify(taxiRank.routes));
+});
--- taxi-rank.js
+++ taxi-rank.js
@@ -1,43 +1,79 @@
-// ROUTE DATA STRUCTURE
-// [{
-//   destination: string,
-//   people: number,
-//   taxis: number,
-//   activity: [taxi arrived @ TIME || taxi departed @ TIME]
-// }]
-
+import moment from 'moment';
 
 export class Route {
   constructor(routes) {
     this._routes = routes;
-  
   }
   addPerson() {
-    this._people++;
+    this._people = [...this._people, new Date()];
+    return this._people.length;
   }
   removePerson() {
-    this._people--;
+    this._people.shift();
+    return this._people.length;
   }
   addTaxi() {
-    this._taxis++;
-    const time = new Date();
-    this._activity = [`Taxi arrived @ ${time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`, ...this._activity];
+    this._taxis = [...this._taxis, new Date()];
+    return {
+      nextTaxi: this._taxis[0]
+        ? moment(this._taxis[0]).format('LT')
+        : undefined,
+      numTaxisWaiting: this._taxis.length,
+    };
   }
   updateQueue() {
-    while (Math.floor(this._people / 11) > 0 && this._taxis > 0) {
-      this._people -= 11;
-      this._taxis--;
-      const time = new Date();
-      this._activity = [`Taxi departed @ ${time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`, ...this._activity];
+    while (Math.floor(this._people.length / 11) > 0 && this._taxis.length > 0) {
+      this._people.splice(0, 11);
+      this._taxis.shift();
+      this._departed = [...this._departed, moment().toISOString()];
     }
-    const routeIndex = this._routes.findIndex(route => route.destination == this._destination);
-    console.log(routeIndex)
+    const routeIndex = this._routes.findIndex(
+      (route) => route.destination == this._destination,
+    );
     this._routes[routeIndex] = {
       destination: this._destination,
       people: this._people,
       taxis: this._taxis,
-      activity: this._activity
-    }
+      departed: this._departed,
+    };
+  }
+
+  avgWaitTime(destination = this._destination) {
+    const queue = this._routes.find(
+      (route) => route.destination == destination,
+    );
+    const milliseconds =
+      queue.people.reduce((sum, time) => {
+        return (
+          sum + moment.duration(moment().diff(moment(time))).asMilliseconds()
+        );
+      }, 0) / queue.people.length;
+    return moment.duration(milliseconds, 'milliseconds').humanize();
+  }
+
+  recentlyDeparted(departed = this._departed) {
+    return departed.filter((time) =>
+      moment(time).isBetween(moment().subtract(1, 'hours'), moment()),
+    );
+  }
+
+  get detailedRoutes() {
+    return this._routes.map((route) => {
+      return {
+        destination: route.destination,
+        numPeople: route.people ? route.people.length : 0,
+        nextTaxi: route.taxis[0]
+          ? moment(route.taxis[0]).format('LT')
+          : undefined,
+        numTaxisWaiting: route.taxis.length,
+        avgWaitTime: route.people[0]
+          ? this.avgWaitTime(route.destination)
+          : 'There are no passengers waiting at this time.',
+        numTaxisDeparted: route.departed
+          ? this.recentlyDeparted(route.departed).length
+          : 0,
+      };
+    });
   }
 
   get routes() {
@@ -45,21 +81,26 @@ export class Route {
   }
 
   set queue(destination) {
-    const queue = this._routes.find(route => route.destination == destination);
+    const queue = this._routes.find(
+      (route) => route.destination == destination,
+    );
     this._destination = queue.destination;
     this._people = queue.people;
     this._taxis = queue.taxis;
-    this._activity = queue.activity;
-
+    this._departed = queue.departed;
   }
 
   get queue() {
     this.updateQueue();
     return {
       destination: this._destination,
-      people: this._people,
-      taxis: this._taxis,
-      activity: this._activity
-    }
+      numPeople: this._people.length,
+      nextTaxi: this._taxis[0]
+        ? moment(this._taxis[0]).format('LT')
+        : undefined,
+      numTaxisWaiting: this._taxis.length,
+      avgWaitTime: this.avgWaitTime(),
+      numTaxisDeparted: this._departed ? this.recentlyDeparted().length : 0,
+    };
   }
 }
